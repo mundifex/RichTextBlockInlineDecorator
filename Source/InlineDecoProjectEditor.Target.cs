e98eedbbc47d2fad4fb403920365bb18dcb45d2e
Ben Donatelli <ben.donatelli@gmail.com>
2025-03-12 11:53:45 -0500

    Update to 5.4

--- Source/InlineDecoProjectEditor.Target.cs
+++ Source/InlineDecoProjectEditor.Target.cs
@@ -8,7 +8,7 @@ public class InlineDecoProjectEditorTarget : TargetRules
 	public InlineDecoProjectEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 
 		ExtraModuleNames.AddRange( new string[] { "InlineDecoProject" } );
 	}
